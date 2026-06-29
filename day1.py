34ff315f87fd0207a96e6bfd72971c387439c916
vikasbanjare672 <vikasbanjare6721@gmail.com>
2026-06-29 23:11:39 +0530

    Create day1.py
    day1 HEE HEE!!!!}

---
+++ day1.py
@@ -0,0 +1,27 @@
+# Exercise: Level 1 :
+#Exercise: Level 2 :
+a , b = 3 , 4
+print(a + b)
+print(a * b)
+print(a - b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(["Asabeneh", "Python", "Finland"]))
+print(type("vikas"))
+print(type({'name':'Asabeneh'}))
+
+
+
+#Exercise: Level 3 :
+
+#Euclidean distance between (2, 3) and (10, 8)
+
+distance = ((10 - 2) ** 2 + (8 - 3) ** 2) ** 0.5
+print("Euclidean distance between (2, 3) and (10, 8) is:", distance)
