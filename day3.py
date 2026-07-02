9c1d9bdc2815e48ffe7eab2cbe5f428ffee1306d
vikasbanjare672 <vikasbanjare6721@gmail.com>
2026-07-02 23:17:51 +0530

    Create day3.py
    BORING!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ day3.py
@@ -0,0 +1,14 @@
+#💻 Exercises - Day 3
+age = int(20)
+height = float(5.9)
+complex_num = 4 + 3j
+
+
+base = int(input("Enter the base of the triangle: "))
+height = int(input("Enter the height of the triangle: "))
+area = 0.5 * base * height
+print("The area of the triangle is:", area)
+
+
+
+
